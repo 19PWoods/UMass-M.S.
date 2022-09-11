ac7703b05fd109729872c930a010f003ca34bb14
Phil Woods <you@example.com19PWoods@gmail.com>
2022-09-11 18:54:59 -0400

    Rates: M6f1

--- Woods_Rate_Fittings_multiple.R
+++ Woods_Rate_Fittings_multiple.R
@@ -78,7 +78,7 @@ names(my_data) <- my_files
 dygraph(my_data$Run2.xlsx)
 
 r2 <- my_data$Run2.xlsx %>% 
-  filter(Time >= 0.068, Time <= 0.2) %>% 
+  filter(Time >= 0.067375, Time <= 0.17) %>% 
   mutate(time0 = Time - Time[[1]], .before = Force_One) %>% 
   select(-Time)
 
@@ -106,12 +106,12 @@ dygraph(r2)
 #              e = r2_phase2_model_summary$estimate[[1]],
 #              g = r2_phase2_model_summary$estimate[[2]]/4)
 
-# grd2 <- list(a = run3_model_tidy$estimate[[1]],
-#              b = run3_model_tidy$estimate[[2]],
-#              c = run3_model_tidy$estimate[[3]],
-#              d = run3_model_tidy$estimate[[4]],
-#              e = run3_model_tidy$estimate[[5]],
-#              g = run3_model_tidy$estimate[[6]])
+grd2 <- list(a = run3_model_tidy$estimate[[1]],
+             b = run3_model_tidy$estimate[[2]],
+             c = run3_model_tidy$estimate[[3]],
+             d = run3_model_tidy$estimate[[4]],
+             e = run3_model_tidy$estimate[[5]],
+             g = run3_model_tidy$estimate[[6]])
 
 ##soleus
 # grd2 <- list(a = 0.005,
@@ -178,7 +178,7 @@ names(run2_info) <- list("Starting Parameters",
 dygraph(my_data$Run3.xlsx)
 
 r3 <- my_data$Run3.xlsx %>% 
-  filter(Time >=0.067625, Time <= 0.2) %>% 
+  filter(Time >=0.067375, Time <= 0.15) %>% 
   mutate(time0 = Time - Time[[1]], .before = Force_One) %>% 
   select(-Time)
 
@@ -262,7 +262,7 @@ names(run3_info) <- list("Starting Parameters",
 dygraph(my_data$Run4.xlsx)
 
 r4 <- my_data$Run4.xlsx %>% 
-  filter(Time >=0.068125, Time <= 0.2) %>% 
+  filter(Time >=0.068, Time <= 0.15) %>% 
   mutate(time0 = Time - Time[[1]], .before = Force_One) %>% 
   select(-Time)
 
@@ -346,7 +346,7 @@ names(run4_info) <- list("Starting Parameters",
 dygraph(my_data$Run5.xlsx)
 
 r5 <- my_data$Run5.xlsx %>% 
-  filter(Time >=0.067, Time <= 0.17) %>% 
+  filter(Time >=0.06725, Time <= 0.13) %>% 
   mutate(time0 = Time - Time[[1]], .before = Force_One) %>% 
   select(-Time)
 
@@ -440,7 +440,7 @@ names(run5_info) <- list("Starting Parameters",
 dygraph(my_data$Run6.xlsx)
 
 r6 <- my_data$Run6.xlsx %>% 
-  filter(Time >=0.067, Time <= 0.17) %>% 
+  filter(Time >=0.067375, Time <= 0.13) %>% 
   mutate(time0 = Time - Time[[1]], .before = Force_One) %>% 
   select(-Time)
 

42527607e8d47c5d402d42263951e7ece93d4887
Phil Woods <you@example.com19PWoods@gmail.com>
2022-12-14 16:48:50 -0500

    made some new scripts

---
+++ Woods_changepoint.R
@@ -0,0 +1,38 @@
+library(tcltk)
+library(tidyverse)
+library(readxl)
+library(changepoint)
+
+
+setwd(tk_choose.dir("Choose X"))
+my_files <- list.files(pattern = "Run")
+my_data <- map(my_files, ~ read_excel(.x, skip = 29) %>%
+                 dplyr::select(Time, Force_One) %>% 
+                 filter(Time <0.5))
+names(my_data) <- my_files
+
+
+## my data
+df <- my_data$Run5.xlsx %>% 
+  filter(Time<0.2)
+plot(df)
+
+df.pm <- cpt.mean(df$Force_One,method = "PELT")
+plot(df.pm, type = "l", cpt.col = "blue", cpt.width = 4)
+
+df.pm <- cpt.meanvar(df$Force_One,method = "PELT")
+plot(df.pm, type = "l", cpt.col = "blue", cpt.width = 4)
+
+## example data from article
+set.seed(10) 
+m.data <- c(rnorm(100, 0, 1), rnorm(100, 1, 1), rnorm(100, 0, 1), + rnorm(100, 0.2, 1)) 
+ts.plot(m.data, xlab = "Index")
+
+m.pelt <- cpt.mean(m.data, method = "PELT")
+plot(m.pelt, type = "l", cpt.col = "blue", cpt.width = 4)
+cpts(m.pelt)
+
+m.pelt.man <- cpt.mean(m.data, penalty = "Manual", pen.value = "1.5 * log(n)",
+                       method = "PELT")
+plot(m.pelt.man, type = "l", cpt.col = "blue", cpt.width = 4)
+cpts(m.pelt.man)
---
+++ Woods_pCa50.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+library(readxl)
+library(writexl)
+
+setwd("C:/Users/Phil/Dropbox/Thesis- Stretch Activation/Data/Woods - Master's Thesis/Project/Tension + AaBbCc")
+
+my_data <- read_excel("SA-Fatigue_Tension+Step+Kinetics_PW_10-28-22.xlsx", 
+                      sheet = "Included",
+                      skip = 5,
+                      na="") %>% 
+  filter(Exp_Con_Num %in% c(2:6)) %>% 
+  filter(Ran_Num == 1) %>% 
+  filter(fiber_type_num %in% c(1:4)) %>% 
+  select(Filename, Muscle,Exp_Con, fiber_type,Po_Pre_Step) 
+
+write_xlsx(my_data, path = "Woods_Fat-Ten.xlsx")

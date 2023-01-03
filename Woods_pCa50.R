4ddb77bd8fd3a094cc5a5ccdb031c95599fa76f6
Phil Woods <you@example.com19PWoods@gmail.com>
2023-01-03 14:52:50 -0500

    added Fsa and FsaF0

--- Woods_pCa50.R
+++ Woods_pCa50.R
@@ -11,6 +11,6 @@ my_data <- read_excel("SA-Fatigue_Tension+Step+Kinetics_PW_10-28-22.xlsx",
   filter(Exp_Con_Num %in% c(2:6)) %>% 
   filter(Ran_Num == 1) %>% 
   filter(fiber_type_num %in% c(1:4)) %>% 
-  select(Filename, Muscle,Exp_Con, fiber_type,Po_Pre_Step) 
+  select(Filename, Muscle,Exp_Con, fiber_type,Po_Pre_Step, Fsa,FsaF0) 
 
 write_xlsx(my_data, path = "Woods_Fat-Ten.xlsx")

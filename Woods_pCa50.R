c6f4c5f86bfd436cc8c99aac0d8159a0b0455bcb
Phil Woods <you@example.com19PWoods@gmail.com>
2023-01-04 09:30:08 -0500

    Made changes to index to include shape =muscle. pCa50 separated into bins

--- Woods_pCa50.R
+++ Woods_pCa50.R
@@ -14,3 +14,16 @@ my_data <- read_excel("SA-Fatigue_Tension+Step+Kinetics_PW_10-28-22.xlsx",
   select(Filename, Muscle,Exp_Con, fiber_type,Po_Pre_Step, Fsa,FsaF0) 
 
 write_xlsx(my_data, path = "Woods_Fat-Ten.xlsx")
+
+
+df2 <- read_excel("Woods_pCa50-Active_vs_Fatigue.xlsx") %>% 
+  mutate(fat_group = case_when(Perc_Active < 35 ~ 'A',
+                               Perc_Active >= 35 & Perc_Active <=60 ~ 'B',
+                               Perc_Active > 60 & Perc_Active <= 80 ~ 'C',
+                               Perc_Active > 80 ~ 'D'),
+         .after = FsaF0)
+
+(gg1 <- ggplot(data = df2, aes(x = fat_group, y = Fsa))+
+    geom_point()
+  
+)
